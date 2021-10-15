b01e3a1f2cc8d04a64753d4231a242652b729164
Pranav Sivadas Menon <42662684+psmenon@users.noreply.github.com>
2021-10-15 01:34:10 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,8 +9,7 @@ from src.prepare import process_data
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    print('run command')
-    if os.system("dvc pull") != 0:
+    if os.system("dvc pull -r s3remote") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
